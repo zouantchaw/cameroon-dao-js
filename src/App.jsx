e9f5c9f7cbaef181be2f2344278f1b582f1733b8
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-12-19 19:41:12 +0000

    add connect to wallet option

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useMemo, useState } from 'react';
+
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  // use connectWallet hook from third web
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+  console.log("provider:", provider)
+
+  // If users wallet is not connected to app, show connect wallet button
+  if (!address) {
+    return (
+      <div className="landing">
+        <h2>Welcome to CameroonDAO</h2>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    )
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected to app... now what</h1>
     </div>
   );
 };
