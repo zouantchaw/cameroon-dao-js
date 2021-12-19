6150bcc0fa68741a34ce254535b435f3a8645a1c
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-12-19 21:20:06 +0000

    claim condition script using third web

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x635a3DE9D799C62CA88fF62a19fC51AA95EA9690",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    // Specify conditions.
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Sucessfully set claim condition!");
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
